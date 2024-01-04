c9ad9e0bdecfe5ff83b54c73ee696809fe816593
kaspher <s24547@pjwstk.edu.pl>
2024-01-04 16:15:30 +0100

    prepared basic client/server view; small changes in tables naming

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const { db } = require('./config/db')
+const cors = require('cors')
+const mysql = require("mysql");
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+app.get("/academies", (req,res)=>{
+    db.query("SELECT * FROM academies", (err,result)=>{
+        err ? console.log(err) : res.send(result)
+    });
+});
+
+app.get("/academies/:id", (req,res)=>{
+    const id = req.params.id;
+    db.query("SELECT * FROM posts WHERE id = ?", id, (err,result)=>{
+        err ? console.log(err) : res.send(result)
+    });
+});
+
+app.listen(3001, () => console.log("Server started on port 3001"))
