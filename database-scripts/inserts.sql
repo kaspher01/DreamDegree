53cfb1786673276b970198af1cd7308fdf3dc9db
s24577 <s24577@pjwstk.edu.pl>
2024-01-05 00:31:43 +0100

    Added-values

---
+++ database-scripts/inserts.sql
@@ -0,0 +1,20 @@
+insert into academy values (1, 1, 1, 'Uniwersytet Warszawski', 'https://www.uw.edu.pl');
+insert into address values (10, 'Warszawa', 'Targowa przy Wileńskiej', '', '03-700');
+insert into accommodation values (1, 'Warszawa Śródmieście', 10, 'pokoj', 1800, 'https://adresowo.pl/o/n0w4a6');
+insert into address values (1, 'Warszawa', 'Krakowskie Przedmiescie', '26/28', '00-927');
+insert into bus_stop values (1, 'Targowa przy wileńskije');
+insert into bus_line values (1, 1, 'N192');
+insert into field_of_study values (1, 'Informatyka');
+insert into field_of_study values (2, 'Matematyka Stosowana');
+insert into field_of_study values (3, 'Metafizyka');
+insert into field_of_study values (4, 'Chemia');
+insert into field_of_study values (5, 'Medycyna');
+insert into transport values (1, 1, 'autobus');
+insert into search_engine values (1, 1, 1, 1);
+
+
+
+
+
+
+
