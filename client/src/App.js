bc18e5a621648ebe6a8d4b92d4ecdef426ad2582
s24577 <s24577@pjwstk.edu.pl>
2024-01-08 00:25:10 +0100

    fixed styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import './styles/App.css';
+import './App.css';
 import Navbar from "./components/Navbar";
 import {Route, Routes} from "react-router-dom";
 import {useEffect, useState} from "react";
 import Academies from "./pages/Academies";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
-import './styles/SchoolList.css';
+import './SchoolList.css';
 
 
 function App() {
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './styles/index.css';
+import './index.css';
 import App from './App';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
