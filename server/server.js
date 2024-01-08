df75d51b1f393611aa00eb7f2117d245334ad839
kubaz <jakub.zawadzki16@gmail.com>
2024-01-08 22:25:31 +0100

    Add user desciption to login token, fix endpoints

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.post("/register", (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    const sql = `INSERT INTO user (name, surname, email, password) VALUES ("${name}", "${surname}", "${email}", "${password}")`;
+    const sql = `INSERT INTO users (name, surname, email, password) VALUES ("${name}", "${surname}", "${email}", "${password}")`;
 
     db.query(sql, function(err) {
         if (err) throw err;
@@ -44,7 +44,7 @@ app.post("/login", (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    const sql = `SELECT email, password FROM user`;
+    const sql = `SELECT id_user, email, password FROM users`;
 
     db.query(sql, function (err,row) {
         let result = Object.values(JSON.parse(JSON.stringify(row)));
@@ -53,7 +53,8 @@ app.post("/login", (req, res) => {
 
         if(matchedUser) {
             res.send({
-                token: 'test123'
+                userId: matchedUser.id_user,
+                userEmail: matchedUser.email
             });
         }
         else {
